f90c7e9e46e6a48f13c0828422f0bb6f36018a15
zhe-ma <mailMazhe@163.com>
2022-01-12 11:07:27 +0800

    Opengl03: fix matrix

--- app/src/main/java/com/example/learnandroid/renderer/TriangleRenderer03.java
+++ app/src/main/java/com/example/learnandroid/renderer/TriangleRenderer03.java
@@ -111,6 +111,10 @@ public class TriangleRenderer03 implements GLSurfaceView.Renderer {
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
 
+        // 参考：
+        // 《OpenGL 学习系列---投影矩阵》https://cloud.tencent.com/developer/article/1472455
+        // 《Android OpenGL ES 4.正交投影》https://www.jianshu.com/p/51a405bc52ed
+
         // 设置相机位置: Matrix.setLookAtM
         // 相机位置：相机的位置是比较好理解的，就是相机在3D空间里面的坐标点。
         // 相机观察方向：相机的观察方向，表示的是相机镜头的朝向，你可以朝前拍、朝后拍、也可以朝左朝右，或者其他的方向。
@@ -122,18 +126,34 @@ public class TriangleRenderer03 implements GLSurfaceView.Renderer {
         //                    float upX,float upY,float upZ)  //up向量在xyz上的分量
         Matrix.setLookAtM(viewMatrix, 0, 0, 0, 7f, 0, 0, 0, 0, 1, 0);
 
+        // 不管是正交投影还是透视投影，最终都是将视景体内的物体投影在近平面上，这也是 3D 坐标转换到 2D 坐标的关键一步。
+
         // 使用正交投影，物体呈现出来的大小不会随着其距离视点的远近而发生变化。
         // Matrix.orthoM (float[] m,  //接收正交投影的变换矩阵
         //                int mOffset,  //变换矩阵的起始位置（偏移量）
-        //                float left,  //相对观察点近面的左边距
-        //                float right,  //相对观察点近面的右边距
-        //                float bottom,  //相对观察点近面的下边距
-        //                float top,  //相对观察点近面的上边距
+        //                float left,  //相对观察点近面的左边距，x的最小值
+        //                float right,  //相对观察点近面的右边距，x的最大值
+        //                float bottom,  //相对观察点近面的下边距，y的最小值
+        //                float top,  //相对观察点近面的上边距，y的最大值
         //                float near,  //相对观察点近面距离
         //                float far)  //相对观察点远面距离
-        float ratio = (float) width / height;
-        // Matrix.orthoM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
-        Matrix.orthoM (projectionMatrix, 0, -ratio * 2, ratio * 2, -2, 2, 3, 10);
+        if (width > height) {
+            // 横屏
+            float ratio = (float) width / (float) height;
+            Matrix.orthoM(projectionMatrix, 0, -ratio, ratio, -1, 1, 1, 10);
+        } else {
+            // 竖屏
+            float ratio = (float)height / (float)width;
+            // left，right, bottom, top 可以理解为当前屏幕归一化的坐标系范围。
+            // 例如如下赋值湿的横坐标的范围是(-1, 1)，纵坐标的范围是(-ratio, ratio)。
+            // 赋值前横纵坐标的范围都是(-1, 1)，但是竖屏高度大于宽度，所以纵轴均分到(-1， 1)的单位长度更长，
+            // 使得画出三角形的纵轴更长。将纵轴的范围改为(-ratio, ratio)，将纵轴划分更多的份数，那么就可以
+            // 将纵轴的范围长度和横轴的一样，这样画出来的横纵则一样。
+             Matrix.orthoM(projectionMatrix, 0, -1, 1, -ratio, ratio, 1, 10);
+
+            // 如下赋值，将坐标轴范围改为(-2 ,2)，三角形的顶点是0.5位置,那么画出来的三角形则是四分之一的长度。
+//            Matrix.orthoM(projectionMatrix, 0, -2, 2, -ratio * 2, ratio * 2, 1, 10);
+        }
 
         // 使用透视投影，物体离视点越远，呈现出来的越小。离视点越近，呈现出来的越大。
         // Matrix.frustumM (float[] m,  //接收透视投影的变换矩阵
